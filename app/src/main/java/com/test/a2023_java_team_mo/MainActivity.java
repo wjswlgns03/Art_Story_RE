737fb40c9007289e563e4657cf79e2f7a24cf570
jjh03 <jjh030726@tukorea.ac.kr>
2023-11-25 11:40:52 +0900

    "test1"

--- app/src/main/java/com/test/a2023_java_team_mo/MainActivity.java
+++ app/src/main/java/com/test/a2023_java_team_mo/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+// test 1
+
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView; // 바텀 네비게이션 뷰
     private FragmentManager fm;
